d723cc03da57fad0968e68f23417938fb9edf931
TwinklerG <231830131@smail.nju.edu.cn>
2025-03-24 17:29:33 +0800

    vue frontend add updateInfo

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -14,6 +14,7 @@ declare module 'vue' {
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElInput: typeof import('element-plus/es')['ElInput']
+    ElLabel: typeof import('element-plus/es')['ElLabel']
     ElMain: typeof import('element-plus/es')['ElMain']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
--- frontend/src/components/AdminHeader.vue
+++ frontend/src/components/AdminHeader.vue
@@ -1,3 +1,7 @@
 <script setup lang="ts"></script>
 
-<template><div>AdminHeader</div></template>
+<template>
+  <el-menu mode="horizontal" :ellipsis="false">
+    <el-menu-item>我的</el-menu-item>
+  </el-menu>
+</template>
