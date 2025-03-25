186a780aec05ead6ad9d35a87e094032f152fb48
TwinklerG <231830131@smail.nju.edu.cn>
2025-03-25 17:17:03 +0800

    add kmp

--- frontend/src/components/AdminHeader.vue
+++ frontend/src/components/AdminHeader.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { router } from "@/route";
+
+const exitAdmin = () => {
+  router.push("/app");
+};
+</script>
 
 <template>
   <el-menu mode="horizontal" :ellipsis="false">
-    <el-menu-item>我的</el-menu-item>
+    <el-sub-menu>
+      <template #title>我的</template>
+      <el-menu-item @click="exitAdmin"> 退出管理员界面 </el-menu-item>
+    </el-sub-menu>
   </el-menu>
 </template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,11 @@
-import "./assets/style.css"
-import 'element-plus/dist/index.css'
-import { createApp } from 'vue'
-import App from './App.vue'
-import { router } from "./route"
-import axios from 'axios';
+import "./assets/style.css";
+import "element-plus/dist/index.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import { router } from "./route";
+import axios from "axios";
 
-axios.defaults.baseURL = ("http://localhost:8080")
+axios.defaults.baseURL = "http://localhost:8080";
 axios.defaults.timeout = 30000;
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
